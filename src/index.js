91b0b70e45506c829840d5e04cd03c180bf5b428
jrshields86 <jrshields86@gmail.com>
2023-11-08 17:52:41 -0700

    displayig data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,74 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 
 const App = ()=> {
+  const [users, setUsers] = useState([]);
+  const [things, setThings] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async() => {
+      const response = await axios.get('/api/users'); 
+      setUsers(response.data);
+    }
+    fetchUsers();
+  }, []);
+
+  useEffect(() => {
+    const fetchThings = async() => {
+      const response = await axios.get('/api/things'); 
+      setThings(response.data);
+    }
+    fetchThings();
+  }, []);
+
   return (
-    <h1>FullStack Template</h1>
+    <div>
+      <h1>Thing Tracker</h1>
+      <main>
+        <div>
+          <h2>Users ( {users.length} )</h2>
+          <ul>
+            {
+              users.map( user => {
+                const usersThings = things.filter(thing => thing.user_id === user.id)
+                return (
+                  <li key={user.id}>
+                    {user.name}
+                    ({usersThings.length})
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </div>
+        <div>
+        <h2>Things ( {things.length} )</h2>
+        <ul>
+          {
+            things.map( thing => {
+              return (
+                <li key={thing.id}>
+                  {thing.name}
+                  <ul>
+                    {
+                      users.map( user => {
+                        return (
+                          <li key={user.id} className={ thing.user_id === user.id ? 'owner': ''}>
+                            {user.name}
+                          </li>
+                        )
+                      })
+                    }
+                  </ul>
+                </li>
+              )
+            })
+          }
+        </ul>
+        </div>
+      </main>
+    </div>
   );
 };
 
